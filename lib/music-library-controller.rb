81f8f781461f41e0362d87264d1df1426d2d3798
Keith Isham <kisham8899@gmail.com>
2019-05-30 13:11:10 +0000

    Automatically backed up by Learn

--- lib/music-library-controller.rb
+++ lib/music-library-controller.rb
@@ -36,7 +36,20 @@ def call
   end
 end
 
-
+def list_songs
+=begin
+  Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1) do |s, i|
+      puts "#{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
+    end
+=end
+ #this gives you an array of song instances sorted by name alphabetically
+  songs_sorted_by_name = Song.all.sort_by do |song|
+    song.name
+  end
+  songs_sorted_by_name.each.with_index(1) do |song,index|
+    puts "#{index}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+  end
+end
 
 
 
